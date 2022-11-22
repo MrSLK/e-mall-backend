8ab558c52ed5cbd39277d77f6bf8f66c42bdc4d5
Shiba Koenaite <koenaite8@gmail.com>
2022-11-23 00:20:43 +0200

    Make it better

--- Controllers/product.js
+++ Controllers/product.js
@@ -145,7 +145,7 @@ module.exports.getOneProduct = (req, res) => {
 
     pool.query(query.text, query.value).then((response) => {
 
-        console.log(response.rows);
+        console.log("Line 148 ->",response.rows);
     let cheaperQuery = {
         text: `select product.id, 
         product.name AS product_name, 

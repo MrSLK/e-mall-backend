c1f70b4d4c7b6d53fbc58643c84923f97a8c9426
MrSLK <51152571+MrSLK@users.noreply.github.com>
2023-05-19 08:06:04 +0200

    Update product.js
    Added console log on controllers/product.js line 139

--- Controllers/product.js
+++ Controllers/product.js
@@ -123,6 +123,7 @@ module.exports.getOneProduct = (req, res) => {
         AND product.category_id = $2`,
         value: [req.body.shop_id, req.body.category_id, response.rows[0].price]
     }
+        console.log("response.rows ->", response.rows)
         if (response.rowCount > 0) {
             pool.query(cheaperQuery.text, cheaperQuery.value).then((result) => {
                 
